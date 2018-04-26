caed06f6748a79b738952183225f7e87a3f9f5a4
Michael Mason <TeaUponTweed@gmail.com>
2018-04-25 21:57:42 -0600

    cleanup of server.py

--- server.py
+++ server.py
@@ -1,25 +1,51 @@
+import sys
+import random
+
 import bottle as b
 # from bottle import route, run, template
+
+from pushfight import EXAMPLE_BOARD
 
 games = {}
 users = {}
 
+
+def init_board():
+    return EXAMPLE_BOARD
+
+
+def validate_user(user):
+    return True
+
+
+def check_cred(user, password):
+    return True
+
+
 def new_anonymous_user():
-    for i in xrange(10000):
+    for i in range(10000):
         if i not in users:
             users[i] = 'anonymous'
             return i
+
 
 def start_new_game(user, name=None):
     if name == 'None':
         name = '{} vs. YOU'.format(users[user])
     return Game(user, name)
 
+
 def get_game_uuid(game):
-    for i in xrange(10000):
+    for i in range(10000):
         if i not in games:
             games[i] = game
             return i
+    print('No slots available! Panic!', file=sys.stderr)
+
+
+def find_game_by_id(gid):
+    return games[gid]
+
 
 class Game(object):
     def __init__(self, user, name):
@@ -35,7 +61,7 @@ class Game(object):
 
         # Pick player colors
         # TODO: Make dict keyed by player id
-        self.colors = rand.sort([1,2])
+        self.colors = random.shuffle([1,2])
 
 
 @b.post('/1/login')
@@ -52,8 +78,8 @@ def login():
     if not is_valid_user:
         b.abort('403', "Login not correct")
 
-    cookie = make_cookie(user)
-    b.response.set_cookie(cookie)
+    # cookie = make_cookie(user)
+    # b.response.set_cookie(cookie)
     b.response.status_code = 204
 
 
@@ -62,29 +88,36 @@ def get_games():
     # Returns: {games: [{game: `<uuid>`, opponent: `<uuid>`, name: "death match"}, ...]}
     game_data = [{
         "game": g.id,
-        "opponent": g.players[0],
+        # "opponent": g.players[0],
+        "players": ",".join(g.players),
         "name": g.name
     } for g in games]
     return {"games": game_data}
 
+def find_open_game():
+    for gid, game in games.items():
+        if len(game.players) == 1:
+            return gid, game
+    return None, None
 
 @b.get('/1/game/start')
 def gameStart_1():
     # Query options: join=`<bool>`, user=`<uuid>`
     # Returns: {game: `<uuid>`, user: `<uuid>`, status: "waiting"|"started", [state: `<boardState>`, color: "white"|"black"]}
-    print b.request.query
+    print(b.request.query)
 
     user = b.request.query.user
     if user:
         # Throw/return error code if bad user
-        validate_user()
+        validate_user(user)
     else:
         user = new_anonymous_user()
 
     retval = {}
     game = None
     if b.request.query.join:
-        game = find_open_game()
+        _, game = find_open_game()
+
     if game is None:
         game = start_new_game(user)
 
@@ -107,15 +140,15 @@ def post_game_join():
     # Returns: {state: `<boardState>`, color: "white"|"black"}
     body = b.request
     gid = body.get('game')
-    if not game:
-        abort(400, 'Bad request - no game id')
     game = games.get(gid)
+    if not game:
+        b.abort(400, 'Bad request - no game id')
     if not game:
         b.abort(404, 'Game {} not found'.format(gid))
 
     user = body.get('user')
     if user:
-        validate_user()
+        validate_user(user)
     else:
         user = new_anonymous_user()
 
