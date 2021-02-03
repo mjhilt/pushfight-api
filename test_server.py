5db52dc94098b52545cc017ecde47793235af3c0
Matt Hilt <mjhilt@gmail.com>
2021-02-02 21:51:53 -0700

    Tabs to spaces - we aren't monsters here

--- test_server.py
+++ test_server.py
@@ -6,44 +6,44 @@ from boddle import boddle
 
 
 def test_login():
-	with boddle(json={'user': "TeaUponTweed@gmail.com"}):
-		# print(b.request.json)
-		try:
-			server.login()
-		except b.HTTPError as e:
-			assert e.status_code == 400
-			# print(e)
+    with boddle(json={'user': "TeaUponTweed@gmail.com"}):
+        # print(b.request.json)
+        try:
+            server.login()
+        except b.HTTPError as e:
+            assert e.status_code == 400
+            # print(e)
 
-	with boddle(json={'user': "TeaUponTweed@gmail.com", 'password': 'wrong'}):
-		# print(b.request.json)
-		try:
-			server.login()
-		except b.HTTPError as e:
-			assert e.status_code == 403
+    with boddle(json={'user': "TeaUponTweed@gmail.com", 'password': 'wrong'}):
+        # print(b.request.json)
+        try:
+            server.login()
+        except b.HTTPError as e:
+            assert e.status_code == 403
 
-	with boddle(json={'user': "TeaUponTweed@gmail.com", 'password': "salted:hashed_password"}):
-		server.login()
-		# TODO can't figure out how to inspect cookies, I'm sure it's fine
+    with boddle(json={'user': "TeaUponTweed@gmail.com", 'password': "salted:hashed_password"}):
+        server.login()
+        # TODO can't figure out how to inspect cookies, I'm sure it's fine
 
 
 def test_opengames():
-	pass
+    pass
 
 def test_game_challenge():
-	pass
+    pass
 
 def test_game_start():
-	pass
+    pass
 
 def test_game_join():
-	pass
+    pass
 
 def test_game_status():
-	pass
+    pass
 
 def test_move():
-	pass
+    pass
 
 
 if __name__ == '__main__':
-	test_login()
+    test_login()
